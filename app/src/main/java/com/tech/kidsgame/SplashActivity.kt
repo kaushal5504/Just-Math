cbc579343afdd5467361c7bcb743388234a7695b
kaushal5504 <kaushalpatel0504@gmail.com>
2023-04-06 20:19:11 +0530

    changes on spalsh

--- app/src/main/java/com/tech/kidsgame/SplashActivity.kt
+++ app/src/main/java/com/tech/kidsgame/SplashActivity.kt
@@ -28,6 +28,7 @@ class SplashActivity : AppCompatActivity() {
 
                     val intent = Intent(this@SplashActivity, MainActivity::class.java)
                     startActivity(intent)
+                    finish()
 
                 }
             }
